2c3d30ae55cbef7434219a0b4edee930d94c614c
Jeremy <70461652+plumpstar@users.noreply.github.com>
2021-01-25 21:57:35 +0800

    Delete 数据处理第二步 directory

--- 数据处理第二步/createFile.py
+++
@@ -1,9 +0,0 @@
-
-
-
-
-for j in range(1, 13):
-    for k in range(1, 32):
-        filename = 'D:\微博数据\分类\\' + str(j) + '月' + str(k) + '日.csv'
-        f = open(file=filename, mode='w', encoding='utf-8')
-        f.close()
